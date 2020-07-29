ee897f06b32e4c029b759037d461b4e3404f8566
Otavio W <59267787+OtavioWc7@users.noreply.github.com>
2020-07-29 00:45:17 -0300

    Add files via upload
    Ao fazer o download do projeto, é necessário a instalação do node_modules

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import RichTextEditor from './components/RichTextEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <RichTextEditor />
+    </div>
+  );
+}
+
+export default App;
